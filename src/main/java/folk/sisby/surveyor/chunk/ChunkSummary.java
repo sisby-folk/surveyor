2078dd62778329e30b40638b9c98dd8a72d2a4fa
sisby-folk <nora.herrah.heartstrings@gmail.com>
2024-02-15 18:16:52 +1100

    use empty compounds for empty layers

--- src/main/java/folk/sisby/surveyor/chunk/ChunkSummary.java
+++ src/main/java/folk/sisby/surveyor/chunk/ChunkSummary.java
@@ -67,17 +67,16 @@ public class ChunkSummary {
         }
     }
 
-    @SuppressWarnings("DataFlowIssue")
     public ChunkSummary(NbtCompound nbt, List<Biome> biomePalette, List<Block> blockPalette) {
         NbtCompound layersCompound = nbt.getCompound(KEY_LAYERS);
         for (String key : layersCompound.getKeys()) {
             int layerY = Integer.parseInt(key);
             FloorSummary[][] layer = new FloorSummary[16][16];
             NbtCompound layerCompound = layersCompound.getCompound(key);
-            int[] heightArray = NbtUtil.readCompressedInts(layerCompound.get(KEY_HEIGHT));
-            int[] biomeArray = NbtUtil.readCompressedInts(layerCompound.get(KEY_BIOME));
-            int[] blockArray = NbtUtil.readCompressedInts(layerCompound.get(KEY_BLOCK));
-            int[] lightArray = NbtUtil.readCompressedInts(layerCompound.get(KEY_LIGHT));
+            int[] heightArray = NbtUtil.readOptionalUInts(layerCompound.get(KEY_HEIGHT));
+            int[] biomeArray = NbtUtil.readOptionalUInts(layerCompound.get(KEY_BIOME));
+            int[] blockArray = NbtUtil.readOptionalUInts(layerCompound.get(KEY_BLOCK));
+            int[] lightArray = NbtUtil.readOptionalUInts(layerCompound.get(KEY_LIGHT));
             for (int i = 0; i < 255; i++) {
                 layer[i / 16][i % 16] = heightArray[i] == -1 ? null : new FloorSummary(layerY - heightArray[i], biomePalette.get(biomeArray[i]), blockPalette.get(blockArray[i]), lightArray[i]);
             }
@@ -89,10 +88,10 @@ public class ChunkSummary {
         NbtCompound layersCompound = new NbtCompound();
         layers.forEach((layerY, floorSummaries) -> {
             NbtCompound layerCompound = new NbtCompound();
-            NbtUtil.writeCompressedInts(layerCompound, KEY_HEIGHT, Arrays.stream(floorSummaries).flatMap(Arrays::stream).map(f -> f == null ? layerY + 1 : f.y()).map(i -> layerY - i).toList());
-            NbtUtil.writeCompressedInts(layerCompound, KEY_BIOME, Arrays.stream(floorSummaries).flatMap(Arrays::stream).map(f -> f == null ? null : biomePalette.indexOf(f.biome())).toList());
-            NbtUtil.writeCompressedInts(layerCompound, KEY_BLOCK, Arrays.stream(floorSummaries).flatMap(Arrays::stream).map(f -> f == null ? null : blockPalette.indexOf(f.block())).toList());
-            NbtUtil.writeCompressedInts(layerCompound, KEY_LIGHT, Arrays.stream(floorSummaries).flatMap(Arrays::stream).map(f -> f == null ? null : f.lightLevel()).toList());
+            NbtUtil.writeOptionalUInts(layerCompound, KEY_HEIGHT, Arrays.stream(floorSummaries).flatMap(Arrays::stream).mapToInt(f -> f == null ? layerY + 1 : f.y()).map(i -> layerY - i).toArray());
+            NbtUtil.writeOptionalUInts(layerCompound, KEY_BIOME, Arrays.stream(floorSummaries).flatMap(Arrays::stream).mapToInt(f -> f == null ? -1 : biomePalette.indexOf(f.biome())).toArray());
+            NbtUtil.writeOptionalUInts(layerCompound, KEY_BLOCK, Arrays.stream(floorSummaries).flatMap(Arrays::stream).mapToInt(f -> f == null ? -1 : blockPalette.indexOf(f.block())).toArray());
+            NbtUtil.writeOptionalUInts(layerCompound, KEY_LIGHT, Arrays.stream(floorSummaries).flatMap(Arrays::stream).mapToInt(f -> f == null ? -1 : f.lightLevel()).toArray());
             layersCompound.put(String.valueOf(layerY), layerCompound);
         });
         nbt.put(KEY_LAYERS, layersCompound);
--- src/main/java/folk/sisby/surveyor/util/NbtUtil.java
+++ src/main/java/folk/sisby/surveyor/util/NbtUtil.java
@@ -7,15 +7,16 @@ import net.minecraft.nbt.NbtElement;
 import net.minecraft.nbt.NbtInt;
 import net.minecraft.nbt.NbtIntArray;
 
+import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
 import java.util.Objects;
 import java.util.stream.IntStream;
 
 public class NbtUtil {
     public static int UINT_OFFSET = 127;
 
-    public static int[] readCompressedInts(NbtElement element) {
+    public static int[] readOptionalUInts(NbtElement element) {
+        if (element == null) return Collections.nCopies(255, -1).stream().mapToInt(i -> i).toArray();
         return switch (element.getType()) {
             case NbtElement.BYTE_TYPE -> Collections.nCopies(255, ((NbtByte) element).intValue() + UINT_OFFSET).stream().mapToInt(i -> i).toArray();
             case NbtElement.BYTE_ARRAY_TYPE -> {
@@ -28,12 +29,14 @@ public class NbtUtil {
         };
     }
 
-    public static void writeCompressedInts(NbtCompound compound, String key, List<Integer> list) {
-        Integer single = list.stream().filter(Objects::nonNull).distinct().count() == 1 ? list.stream().filter(Objects::nonNull).distinct().findFirst().get() : null;
-        boolean oneByte = list.stream().filter(Objects::nonNull).allMatch(i -> i >= -128 + UINT_OFFSET && i < 127 + UINT_OFFSET);
+    public static void writeOptionalUInts(NbtCompound compound, String key, int[] array) {
+        long distinct = Arrays.stream(array).filter(i -> i >= 0).distinct().count();
+        if (distinct == 0) return;
+        Integer single = distinct == 1 ? Arrays.stream(array).filter(i -> i >= 0).distinct().findFirst().getAsInt() : null;
+        boolean oneByte = Arrays.stream(array).filter(Objects::nonNull).allMatch(i -> i >= -128 + UINT_OFFSET && i < 127 + UINT_OFFSET);
         if (single != null && oneByte) compound.putByte(key, (byte) (single - UINT_OFFSET));
-        if (single == null && oneByte) compound.putByteArray(key, list.stream().map(i -> Objects.requireNonNullElse(i, -1)).map((i -> (byte) (i - UINT_OFFSET))).toList());
+        if (single == null && oneByte) compound.putByteArray(key, Arrays.stream(array).mapToObj((i -> (byte) (i - UINT_OFFSET))).toList());
         if (single != null && !oneByte) compound.putInt(key, single);
-        if (single == null && !oneByte) compound.putIntArray(key, list.stream().map(i -> Objects.requireNonNullElse(i, -1)).toList());
+        if (single == null && !oneByte) compound.putIntArray(key, array);
     }
 }
