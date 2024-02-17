bd2dc398cfa347dc2efbd0c7d246cb7731496283
sisby-folk <nora.herrah.heartstrings@gmail.com>
2024-02-18 10:49:56 +1100

    fix nether ceiling

--- src/main/java/folk/sisby/surveyor/chunk/DimensionSupport.java
+++ src/main/java/folk/sisby/surveyor/chunk/DimensionSupport.java
@@ -11,7 +11,7 @@ public class DimensionSupport {
         TreeSet<Integer> set = new TreeSet<>();
         DimensionType dimension = world.getDimension();
         set.add(dimension.minY() + dimension.height() - 1); // Layer at Max Y
-        set.add(dimension.minY() + dimension.logicalHeight() - 1); // Layer At Playable Limit
+        set.add(dimension.minY() + dimension.logicalHeight() - 2); // Layer below Playable Limit
         if (dimension.hasSkyLight()) set.add(world.getSeaLevel() - 2); // Layer below sea level (assume caves underneath)
         if (dimension.minY() + dimension.height() > 256) set.add(256); // Layer At Y=256 (assume special layer change)
         if (dimension.minY() < 0) set.add(0); // Layer At Y=0 (assume special layer change)
