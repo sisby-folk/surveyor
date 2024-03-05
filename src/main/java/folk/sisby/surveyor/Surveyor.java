0dd4a41721e18435f1441ad6ef1580e733bbca74
sisby-folk <nora.herrah.heartstrings@gmail.com>
2024-03-05 13:34:25 +1100

    separate structure check for raycast

--- src/main/java/folk/sisby/surveyor/Surveyor.java
+++ src/main/java/folk/sisby/surveyor/Surveyor.java
@@ -18,6 +18,7 @@ import net.minecraft.util.Formatting;
 import net.minecraft.util.WorldSavePath;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
+import net.minecraft.world.World;
 import net.minecraft.world.chunk.ChunkStatus;
 import net.minecraft.world.dimension.DimensionType;
 import net.minecraft.world.gen.structure.Structure;
@@ -38,6 +39,34 @@ public class Surveyor implements ModInitializer {
         return DimensionType.getSaveDirectory(world.getRegistryKey(), world.getServer().getSavePath(WorldSavePath.ROOT)).resolve(DATA_SUBFOLDER).resolve(Surveyor.ID).toFile();
     }
 
+    public static void checkStructureExploration(ServerWorld world, ServerPlayerEntity player, BlockPos pos) {
+        SurveyorPlayer sp = (SurveyorPlayer) player;
+        Map<Structure, LongSet> structureReferences = world.getChunk(pos.getX() >> 4, pos.getZ() >> 4, ChunkStatus.STRUCTURE_REFERENCES).getStructureReferences();
+        if (!structureReferences.isEmpty()) {
+            structureReferences.forEach((structure, chunkPosSet) -> {
+                LongSet unexploredSet = new LongArraySet(chunkPosSet.toLongArray());
+                if (sp.surveyor$getExploredStructures().containsKey(structure)) unexploredSet.removeAll(sp.surveyor$getExploredStructures().get(structure));
+                for (Long longPos : unexploredSet) {
+                    ChunkPos startPos = new ChunkPos(longPos);
+                    StructureStart start = world.getChunk(startPos.x, startPos.z, ChunkStatus.STRUCTURE_STARTS).getStructureStart(structure);
+                    boolean found = false;
+                    if (start.getBoundingBox().contains(pos)) {
+                        for (StructurePiece piece : start.getChildren()) {
+                            if (piece.getBoundingBox().contains(pos)) {
+                                sp.surveyor$addExploredStructure(structure, startPos);
+                                found = true;
+                                break;
+                            }
+                        }
+                    }
+                    if (found && CONFIG.debugMode) {
+                        player.sendMessageToClient(Text.literal("Discovered ").append(Text.literal(StringUtils.capitalize(world.getRegistryManager().get(RegistryKeys.STRUCTURE).getId(structure).getPath().replace("_", " "))).formatted(Formatting.GREEN)).append(Text.literal(" at ")).append(Text.literal("[%s,%s]".formatted(startPos.x << 4, startPos.z << 4)).formatted(Formatting.GOLD)).formatted(Formatting.GRAY), true);
+                    }
+                }
+            });
+        }
+    }
+
     @Override
     public void onInitialize() {
         SurveyorNetworking.init();
@@ -46,41 +75,8 @@ public class Surveyor implements ModInitializer {
         ServerTickEvents.END_WORLD_TICK.register((world -> {
             if ((world.getTime() & 7) != 0) return;
             for (ServerPlayerEntity player : world.getPlayers()) {
-                Map<Structure, LongSet> structureReferences = world.getChunk(player.getChunkPos().x, player.getChunkPos().z, ChunkStatus.STRUCTURE_REFERENCES).getStructureReferences();
-                if (!structureReferences.isEmpty()) {
-                    SurveyorPlayer sp = (SurveyorPlayer) player;
-                    BlockPos viewPos = BlockPos.ofFloored(player.raycast(16 << 4, 1.0F, false).getPos());
-                    structureReferences.forEach((structure, chunkPosSet) -> {
-                        LongSet unexploredSet = new LongArraySet(chunkPosSet.toLongArray());
-                        if (sp.surveyor$getExploredStructures().containsKey(structure)) unexploredSet.removeAll(sp.surveyor$getExploredStructures().get(structure));
-                        for (Long longPos : unexploredSet) {
-                            ChunkPos pos = new ChunkPos(longPos);
-                            StructureStart start = world.getChunk(pos.x, pos.z, ChunkStatus.STRUCTURE_STARTS).getStructureStart(structure);
-                            boolean found = false;
-                            if (start.getBoundingBox().contains(player.getBlockPos())) {
-                                for (StructurePiece piece : start.getChildren()) {
-                                    if (piece.getBoundingBox().contains(player.getBlockPos())) {
-                                        sp.surveyor$addExploredStructure(structure, pos);
-                                        found = true;
-                                        break;
-                                    }
-                                }
-                            }
-                            if (!found && start.getBoundingBox().contains(viewPos)) {
-                                for (StructurePiece piece : start.getChildren()) {
-                                    if (piece.getBoundingBox().contains(viewPos)) {
-                                        sp.surveyor$addExploredStructure(structure, pos);
-                                        found = true;
-                                        break;
-                                    }
-                                }
-                            }
-                            if (found && CONFIG.debugMode) {
-                                player.sendMessageToClient(Text.literal("Discovered ").append(Text.literal(StringUtils.capitalize(world.getRegistryManager().get(RegistryKeys.STRUCTURE).getId(structure).getPath().replace("_", " "))).formatted(Formatting.GREEN)).append(Text.literal(" at ")).append(Text.literal("[%s,%s]".formatted(pos.x << 4, pos.z << 4)).formatted(Formatting.GOLD)).formatted(Formatting.GRAY), true);
-                            }
-                        }
-                    });
-                }
+                checkStructureExploration(world, player, player.getBlockPos());
+                checkStructureExploration(world, player, BlockPos.ofFloored(player.raycast(16 << 4, 1.0F, false).getPos()));
             }
         }));
     }
