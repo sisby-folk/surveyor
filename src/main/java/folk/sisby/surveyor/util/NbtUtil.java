3ba076c7a80122fc11ba31dc8838965dbc3d6ab9
sisby-folk <nora.herrah.heartstrings@gmail.com>
2024-04-01 10:59:10 +1100

    fix text stripping

---
+++ src/main/java/folk/sisby/surveyor/util/NbtUtil.java
@@ -0,0 +1,21 @@
+package folk.sisby.surveyor.util;
+
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtElement;
+
+import java.util.Collection;
+
+public class NbtUtil {
+    public static void removeRecursive(NbtCompound nbt, Collection<String> keys) {
+        keys.forEach(nbt::remove);
+        for (String key : nbt.getKeys()) {
+            if (nbt.contains(key, NbtElement.COMPOUND_TYPE)) {
+                removeRecursive(nbt.getCompound(key), keys);
+            } else if (nbt.contains(key, NbtElement.LIST_TYPE)) {
+                for (NbtElement listNbt : nbt.getList(key, NbtElement.COMPOUND_TYPE)) {
+                    removeRecursive((NbtCompound) listNbt, keys);
+                }
+            }
+        }
+    }
+}
--- src/main/java/folk/sisby/surveyor/util/TextUtil.java
+++ src/main/java/folk/sisby/surveyor/util/TextUtil.java
@@ -1,9 +1,16 @@
 package folk.sisby.surveyor.util;
 
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtOps;
 import net.minecraft.text.Text;
+import net.minecraft.util.dynamic.Codecs;
+
+import java.util.List;
 
 public class TextUtil {
     public static Text stripInteraction(Text text) {
-        return text.copy().styled(s -> s.withClickEvent(null).withHoverEvent(null).withInsertion(null));
+        NbtCompound nbt = (NbtCompound) Codecs.TEXT.encodeStart(NbtOps.INSTANCE, text).getOrThrow(false, null);
+        NbtUtil.removeRecursive(nbt, List.of("hoverEvent", "clickEvent", "insertion"));
+        return Codecs.TEXT.decode(NbtOps.INSTANCE, nbt).getOrThrow(false, null).getFirst();
     }
 }
