a10c32158df3f3d38e4c601ab09b71bb88d76c8d
sisby-folk <nora.herrah.heartstrings@gmail.com>
2024-05-03 16:32:50 +1000

    implement structure packet splitting

--- src/main/java/folk/sisby/surveyor/packet/S2CStructuresAddedPacket.java
+++ src/main/java/folk/sisby/surveyor/packet/S2CStructuresAddedPacket.java
@@ -1,10 +1,13 @@
 package folk.sisby.surveyor.packet;
 
+import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Multimap;
 import folk.sisby.surveyor.Surveyor;
 import folk.sisby.surveyor.WorldSummary;
 import folk.sisby.surveyor.structure.WorldStructureSummary;
+import folk.sisby.surveyor.util.BitSetUtil;
 import folk.sisby.surveyor.util.MapUtil;
+import io.netty.buffer.Unpooled;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.util.Identifier;
@@ -12,6 +15,10 @@ import net.minecraft.util.math.ChunkPos;
 import net.minecraft.world.World;
 import net.minecraft.world.gen.structure.Structure;
 
+import java.util.ArrayList;
+import java.util.BitSet;
+import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -36,6 +43,30 @@ public record S2CStructuresAddedPacket(boolean shared, Multimap<RegistryKey<Stru
     public void writeBuf(PacketByteBuf buf) {
         buf.writeBoolean(shared);
         summary.writeBuf(buf, keySet);
+    }
+
+    @Override
+    public Collection<PacketByteBuf> toBufs() {
+        List<PacketByteBuf> bufs = new ArrayList<>();
+        PacketByteBuf buf = new PacketByteBuf(Unpooled.buffer());
+        writeBuf(buf);
+        if (buf.readableBytes() < MAX_PAYLOAD_SIZE) {
+            bufs.add(buf);
+        } else {
+            if (keySet.size() == 1) throw new RuntimeException("Couldn't create a structure update packet - an individual structure would be too large to send!");
+            Multimap<RegistryKey<Structure>, ChunkPos> firstHalf = HashMultimap.create();
+            Multimap<RegistryKey<Structure>, ChunkPos> secondHalf = HashMultimap.create();
+            keySet.forEach((key, pos) -> {
+                if (firstHalf.size() < keySet.size()) {
+                    firstHalf.put(key, pos);
+                } else {
+                    secondHalf.put(key, pos);
+                }
+            });
+            bufs.addAll(new S2CStructuresAddedPacket(shared, firstHalf, summary).toBufs());
+            bufs.addAll(new S2CStructuresAddedPacket(shared, secondHalf, summary).toBufs());
+        }
+        return bufs;
     }
 
     @Override
