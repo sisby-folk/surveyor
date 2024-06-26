b581382879600f28517c5c9ff4ffc4fb33c76b21
sisby-folk <nora.herrah.heartstrings@gmail.com>
2024-06-26 10:43:44 +1000

    bump kaleido

--- src/main/java/folk/sisby/surveyor/SurveyorConfig.java
+++ src/main/java/folk/sisby/surveyor/SurveyorConfig.java
@@ -6,18 +6,18 @@ import folk.sisby.kaleido.lib.quiltconfig.api.annotations.IntegerRange;
 
 public class SurveyorConfig extends WrappedConfig {
     @Comment("Various debug loggers and messages")
-    public final Boolean debugMode = false;
+    public Boolean debugMode = false;
 
     @Comment("Whether to share all terrain exploration all the time")
-    public final Boolean shareAllTerrain = false;
+    public Boolean shareAllTerrain = false;
 
     @Comment("Whether to share all structure exploration all the time")
-    public final Boolean shareAllStructures = false;
+    public Boolean shareAllStructures = false;
 
     @Comment("Whether to share all landmarks all the time")
-    public final Boolean shareAllLandmarks = false;
+    public Boolean shareAllLandmarks = false;
 
     @Comment("How many ticks to wait before sending the new location of player 'friends'")
     @IntegerRange(min = 1, max = 200)
-    public final Integer ticksPerFriendUpdate = 2;
+    public Integer ticksPerFriendUpdate = 2;
 }
